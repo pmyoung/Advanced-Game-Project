c22336101e7611dfc171bdb9bcf0186dd2540d37
eduardo4jesus <eduardo4jesus@gmail.com>
2013-02-11 11:45:25 -0500

    Update Logic/MatchConfig.cs

--- Logic/MatchConfig.cs
+++ Logic/MatchConfig.cs
@@ -127,3 +127,33 @@ namespace SpaceShip
 
     }
 }
+
+
+
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceShip.Logic
+{
+    class MatchConfig
+    {
+        static public List<Player> players = new List<Player>();      
+        static public Map map;
+        static public int maxPlayers = 2;
+        static public float gravityConstant = 5;
+
+        static public int bulletSpeed = 5;
+        static public int maxHealth = 30;
+        static public int accelerationRate = 2;
+        static public int rotationRate = 2;
+        static public double brakeRate = 0.9;
+        static public int mass = 10;
+    }
+}
