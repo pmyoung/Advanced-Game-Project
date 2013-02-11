7c31caa670bf7312cd1f81f54dc90484caf418a8
eduardo4jesus <eduardo4jesus@gmail.com>
2013-02-11 11:50:12 -0500

    Update Logic/Player.cs

--- Logic/Player.cs
+++ Logic/Player.cs
@@ -80,3 +80,86 @@ namespace SpaceShip
           }
     }
 }
+
+
+
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//SERVERSIDE
+//Contains player data
+//Modifies player coordinates based on the keys depressed by the player
+
+namespace SpaceShip.Logic
+{
+    class Player : GameObject
+    {
+        int id;
+        int bulletId = 0;
+        int invisible;
+        string name;
+
+        
+
+        public Player(int id, string name, int spriteId)
+        {
+            this.id = id;
+            this.name = name;
+            setSpriteId(spriteId);
+        }
+
+        public int getId()
+        {
+            return this.id;
+        }
+
+        public string getName()
+        {
+            return this.name;
+        }
+
+        public void accelerate()
+        {
+            //called whenever the user has 'up' key depressed
+            speedX += (int)(Math.Sin(angle) * MatchConfig.accelerationRate);
+            speedY += (int)(Math.Cos(angle) * MatchConfig.accelerationRate);
+        }
+        public void brake()
+        {
+            //called whenever the user has 'down' key depressed
+            speedX = (int)(speedX * MatchConfig.brakeRate);
+            speedY = (int)(speedY * MatchConfig.brakeRate);
+        }
+        public void rotateLeft()
+        {
+            //called whenever the player has the 'right' key depressed
+            angle -= MatchConfig.rotationRate;
+        }
+        public void rotateRight()
+        {
+            //called whenever the player has the 'left' key depressed
+            angle += MatchConfig.rotationRate;
+        }
+        public void fire()
+          {
+              //give the bullet the player's initial speed, plus a component 
+              float bSpeedX = (float)(this.speedX + Math.Sin(this.angle) * MatchConfig.bulletSpeed);
+              float bSpeedY = (float)(this.speedY + Math.Cos(this.angle) * MatchConfig.bulletSpeed);
+              //
+              float bX = (float)(this.x + Math.Sin(this.angle) * this.radius);
+              float bY = (float)(this.y + Math.Cos(this.angle) * this.radius);
+
+              //called whenever the player has the 'space' key depressed
+              new Bullet(bX,bY,bSpeedX,bSpeedY, id, bulletId);
+          }
+
+        
+    }
+}
