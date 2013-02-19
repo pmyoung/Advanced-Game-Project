3133ed4450bd0aad82fc26548afd491ebbb56866
Patrick Young <pmyoung@lakeheadu.ca>
2013-02-18 22:54:59 -0500

    New General classes
    These 2 classes are for general graphics use

---
+++ Graphics/GraphicsModel.cs
@@ -0,0 +1,91 @@
+﻿/*
+ * @author Patrick Young
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameGraphics
+{
+    class GraphicsModel
+    {
+        // dictionary key is the unique ID of the given GraphicsObject which is the same GetID()
+        private Dictionary<int, GraphicsObject> dictionary;
+
+        /*
+         * Default Constructor that initializes the dictionary
+         */
+        public GraphicsModel()
+        {
+            this.dictionary = new Dictionary<int, GraphicsObject>();
+        }// GraphicsModel
+
+        /*
+         * Update takes a GraphicsObject as a parameter. If there is an object with the
+         * same ID we will update the existing object with the new info otherwise we add
+         * the object to our dictionary of object
+         * 
+         * @param
+         * obj: The object that needs to be updated
+         */
+        public void Update(GraphicsObject obj)
+        {
+            GraphicsObject value;
+            if (this.dictionary.TryGetValue(obj.GetID(), out value))
+            {
+                // we have it so we just need to update it
+                value.SetX(obj.GetX());
+                value.SetY(obj.GetY());
+                value.SetAngle(obj.GetAngle());
+            }
+            else
+            {
+                // we dont have it so we need to add it
+                this.dictionary.Add(obj.GetID(), obj);
+            }
+        }// Update
+
+
+        /*
+         * When called will return a reference to the dictionary it uses
+         */
+        public Dictionary<int, GraphicsObject> GetAsDictionary()
+        {
+            return this.dictionary;
+        }// GetAsDictionary
+
+        /*
+         * When called wil return the dictionary in a List format of its values
+         * to allow iteration.
+         */
+        public List<GraphicsObject> GetAsList()
+        {
+            List<GraphicsObject> list = new List<GraphicsObject>(this.dictionary.Values);
+            return list;
+        }// GetAsList
+
+        /*
+         * Returns the GraphicsObject with the given ID
+         * if no object with this ID exists then it returns null
+         * 
+         * @param
+         * id: the ID of the GraphicsObject
+         */
+        public GraphicsObject GetObjectWithID(int id)
+        {
+            GraphicsObject obj;
+
+            if (this.dictionary.TryGetValue(id, out obj))
+            {
+                // we have it
+                return obj;
+            }
+            else
+            {
+                // we dont have it
+                return null;
+            }
+        }// GetObjectWithID
+    }// GraphicsModel Class
+}
---
+++ Graphics/GraphicsObject.cs
@@ -0,0 +1,226 @@
+﻿/*
+ * @author Patrick Young
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+
+namespace GameGraphics
+{
+    class GraphicsObject
+    {
+        private static int DEFAULT_COLOR = 0;
+
+        private static Color[] COLOR = { Color.White, Color.LightCoral, Color.LightBlue, Color.Lime, Color.Yellow, Color.Cyan, Color.Orange, Color.Plum, Color.Pink };
+
+        private int id;
+        private int spriteID;
+        private int colorID;
+        
+        private float x;
+        private float y;
+        private float radius;
+        private float angle;
+
+        /*
+         * This constructor creates a GraphicsObject with all its parameters defined with values
+         * 
+         * @param
+         * id: the ID of the object this should be unique
+         * x: the x location of the object
+         * y: the y location of the object
+         * radius: the radius of the object
+         * angle: the angle of the object
+         * spriteID: the ID of the sprite the object uses
+         * colorID: the ID of the color it wil have (used for the player sprites)
+         */
+        public GraphicsObject(int id, float x, float y, float radius, float angle, int spriteID, int colorID)
+        {
+            this.SetID(id);
+            this.SetX(x);
+            this.SetY(y);
+            this.SetRadius(radius);
+            this.SetAngle(angle);
+            this.SetSpriteID(spriteID);
+            this.SetColorID(colorID);
+        }// GraphicsObject
+
+        /*
+         * Constructor that accepts all but the colorID as parameters. This is usefull
+         * for object that do not need their color pallet changed (all objects other
+         * than the players)
+         * 
+         * @param
+         * id: the ID of the object this should be unique
+         * x: the x location of the object
+         * y: the y location of the object
+         * radius: the radius of the object
+         * angle: the angle of the object
+         * spriteID: the ID of the sprite the object uses
+         */
+        public GraphicsObject(int id, int x, int y, int radius, int angle, int spriteID)
+        {
+            this.SetID(id);
+            this.SetX(x);
+            this.SetY(y);
+            this.SetRadius(radius);
+            this.SetAngle(angle);
+            this.SetSpriteID(spriteID);
+            this.SetColorID(DEFAULT_COLOR);
+        }// GraphicsObject
+
+        /*
+         * Sets the ID of the GraphicsObject
+         * 
+         * @param
+         * id: id to be given to the object
+         */
+        public void SetID(int id)
+        {
+            this.id = id;
+        }// SetID
+
+        /*
+         * Sets the X location of the GraphicsObject
+         * 
+         * @param
+         * x: x location to be given to the object
+         */
+        public void SetX(float x){
+            this.x = x;
+        }// SetX
+
+        /*
+         * Sets the Y location of the GraphicsObject
+         * 
+         * @param
+         * y: y location to be given to the object
+         */
+        public void SetY(float y)
+        {
+            this.y = y;
+        }// SetY
+
+        /*
+         * Sets the radius of the GraphicsObject
+         * 
+         * @param
+         * r: the radius to be given to the object
+         */
+        public void SetRadius(float r)
+        {
+            this.radius = r;
+        }// SetRadius
+
+        /*
+         * Sets the angle of the GraphicsObject
+         * 
+         * @param
+         * a: the angle to be given to the object
+         */
+        public void SetAngle(float a)
+        {
+            this.angle = a%360;
+        }// SetAngle
+
+        /*
+         * Sets the spriteID which will determine what sprite will be used
+         * for this object
+         * 
+         * @param
+         * spriteID: the ID of the sprite to be used
+         */
+        public void SetSpriteID(int spriteID)
+        {
+            this.spriteID = spriteID;
+        }// SetSpriteID
+
+        /*
+         * Sets the colorID of the GraphicsObject which determines the color.
+         * If the ID is not in the range of the colorID's then it will use the
+         * default (White)
+         * 
+         * @param
+         * colorID: the ID value of the color to be used
+         */
+        public void SetColorID(int colorID)
+        {
+            if (colorID > 0 && colorID < COLOR.Length)
+            {
+                this.colorID = colorID;
+            }
+            else // if we get a value that is not in our range make it white
+            {
+                this.colorID = DEFAULT_COLOR;
+            }
+        }// SetColorID
+
+        /*
+         * Returns the objects unique ID.
+         */
+        public int GetID()
+        {
+            return this.id;
+        }// GetID
+
+        /*
+         * Returns the given X location
+         */
+        public float GetX()
+        {
+            return this.x;
+        }// GetX
+
+        /*
+         * Returns the given Y location
+         */
+        public float GetY()
+        {
+            return this.y;
+        }// GetY
+
+        /*
+         * Returns the given radius of the object
+         */
+        public float GetRadius()
+        {
+            return this.radius;
+        }// GetRadius
+
+        /*
+         * Returns the given angle of the object
+         */
+        public float GetAngle()
+        {
+            return this.angle;
+        }// GetAngle
+
+        /*
+         * Returns the given sprite ID of the object
+         */
+        public int GetSpriteID()
+        {
+            return this.spriteID;
+        }// GetSpriteID
+
+        /*
+         * Returns the given color ID of the object
+         */
+        public int GetColorID()
+        {
+            return this.colorID;
+        }// GetColorID
+
+        /*
+         * Returns the actual color that this sprite will be using
+         */
+        public Color GetColor()
+        {
+            return COLOR[this.GetColorID()];
+        }// GetColor
+
+    }// GrahicsObject
+}
